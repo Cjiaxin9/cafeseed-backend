ef92b1a363c52405dd7e5078441c3c7e0039f4ef
Cjiaxin9 <110971207+Cjiaxin9@users.noreply.github.com>
2022-12-01 01:59:43 +0800

    Add files via upload

--- db/db.js
+++ db/db.js
@@ -2,16 +2,8 @@ const mongoose = require("mongoose");
 
 const connectDB = async (uri) => {
   try {
-    
-      const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true
-    });
-    
-    console.log(`mongo database is connected!!! ${conn.connection.host} `);
-//     await mongoose.connect(uri);
-//     console.log("DB connected");
+    await mongoose.connect(uri);
+    console.log("DB connected");
   } catch (err) {
     console.error(err.message);
     process.exit(1);
