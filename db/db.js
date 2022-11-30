43c30991bec337543563a01417248ea57472d53b
Cjiaxin9 <110971207+Cjiaxin9@users.noreply.github.com>
2022-12-01 01:40:59 +0800

    Update db.js

--- db/db.js
+++ db/db.js
@@ -2,8 +2,16 @@ const mongoose = require("mongoose");
 
 const connectDB = async (uri) => {
   try {
-    await mongoose.connect(uri);
-    console.log("DB connected");
+    
+        const conn = await mongoose.connect(process.env.MONGO_URI, {
+      //must add in order to not get any error masseges:
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useCreateIndex: true,
+    });
+    console.log(`mongo database is connected!!! ${conn.connection.host} `);
+//     await mongoose.connect(uri);
+//     console.log("DB connected");
   } catch (err) {
     console.error(err.message);
     process.exit(1);
